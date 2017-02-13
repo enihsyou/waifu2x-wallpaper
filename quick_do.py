5285ea2bdb4d4207c2221da8f6a916feac68c364
九条涼果 <s1131234@gmail.com>
2017-02-14 01:52:48 +0800

    修正序号，提高运行效率，增加注释和提示

--- quick_do.py
+++ quick_do.py
@@ -9,56 +9,70 @@ from shutil import copyfile
 from PIL import Image
 
 running = r"D:\waifu2x-caffe\waifu2x-caffe-cui.exe"
-for no, file in enumerate(os.listdir(os.getcwd()), 1):
-    file_path = os.path.join(os.getcwd(), file)
+# 创建目标文件夹
+output_folder = os.path.join('D:\\', 'temp')
+try:
+    os.mkdir(output_folder)
+    print("目标文件夹: {} 已创建".format(output_folder))
+except FileExistsError:
+    print("目标文件夹: {} 已存在".format(output_folder))
+
+# 处理数量计数器
+no = 0
+
+for file in os.listdir(os.getcwd()):
+    file_path = os.path.join(os.getcwd(), file)  # 来源文件路径
     if not os.path.isfile(file_path):
         continue
-    if os.path.splitext(file)[-1] not in ['.bmp', '.jpeg', '.jpg', '.png']:
+    if os.path.splitext(file)[-1] not in ('.jpg', '.png', '.bmp', '.jpeg'):
         continue
 
     command = [running]
+    no += 1
 
-    try:
-        os.mkdir(os.path.join('D:\\', 'temp'))
-    except FileExistsError:
-        pass
     with Image.open(file) as image:  # type: Image.Image
         width, height = image.size
+
+    if height and width:
         ratio = width / height
-        magnitude = None
-        command.append("-i \"%s\"" % file_path)
-        command.append("-o \"%s\"" % os.path.join('D:\\', 'temp', file))
-        command.append("-p %s" % 'cudnn')
-        if ratio <= 5 / 4:  # 是否是纵向
-            if width < 1440 or height < 2560:
-                t_ratio = 1440 / width  # 横向长度比例
+    else:
+        print("#{:d}: 图片维度信息错误\n\t文件名: {}".format(no, file))
+        continue
 
-                if height * t_ratio >= 2560:
-                    command.append("-w %d" % 1440)
-                    magnitude = t_ratio
-                else:
-                    command.append("-h %d" % 2560)
-                    magnitude = 2560 / height
-        else:
-            if width < 3840 or height < 2160:
-                t_ratio = 3840 / width  # 横向长度比例
+    magnitude = 1
+    command.append("-i \"%s\"" % file_path)
+    command.append("-o \"%s\"" % os.path.join('D:\\', 'temp', file))
+    command.append("-p %s" % 'cudnn')
+    if ratio <= 5 / 4:  # 是否是纵向
+        if width < 1440 or height < 2560:
+            t_ratio = 1440 / width  # 横向长度比例
 
-                if height * t_ratio >= 2160:
-                    command.append("-w %d" % 3840)
-                    magnitude = t_ratio
-                else:
-                    command.append("-h %d" % 2160)
-                    magnitude = 2160 / height
+            if height * t_ratio >= 2560:
+                command.append("-w %d" % 1440)
+                magnitude = t_ratio
+            else:
+                command.append("-h %d" % 2560)
+                magnitude = 2560 / height
+    else:
+        if width < 3840 or height < 2160:
+            t_ratio = 3840 / width  # 横向长度比例
 
-        if len(command) > 4:
-            print("#{:d}: 原始分辨率: ({:d}, {:d}) 比例: {:.4f} 放大率: {:.4f}\n\t文件名: {}".format(
-                no, width, height, ratio, magnitude, file))
-            command = " ".join(command)
-            command_return = subprocess.Popen(command,
-                                              shell=True, stdout=subprocess.PIPE).stdout.read()
-            print("\t{}".format(command_return.decode('shift-jis')))
-        else:
-            print("#{:d}: 原始分辨率: ({:d}, {:d}) 比例: {:.4f}\n\t文件名: {}".format(
-                no, width, height, ratio, magnitude, file))
-            copyfile(file, os.path.join('D:\\', 'temp', file))
-            print("\t无需处理，跳过")
+            if height * t_ratio >= 2160:
+                command.append("-w %d" % 3840)
+                magnitude = t_ratio
+            else:
+                command.append("-h %d" % 2160)
+                magnitude = 2160 / height
+
+    if len(command) > 4:
+        print("#{:d}: 原始分辨率: ({:d}, {:d}) 比例: {:.4f} 放大率: {:.4f}\n\t文件名: {}".format(
+            no, width, height, ratio, magnitude, file))
+        command = " ".join(command)
+        command_return = subprocess.Popen(command,
+                                          shell=True, stdout=subprocess.PIPE).stdout.read()
+        print("\t{}".format(command_return.decode('shift-jis')))
+    else:
+        print("#{:d}: 原始分辨率: ({:d}, {:d}) 比例: {:.4f}\n\t文件名: {}".format(
+            no, width, height, ratio, file))
+        copyfile(file, os.path.join('D:\\', 'temp', file))
+        print("\t无需处理，跳过")
